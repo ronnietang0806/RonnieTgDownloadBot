77dbfe136c85c7f3879e2d4cd297a175fa6049af
RonnieTang <ronnie.tangchipong@gmail.com>
2022-07-30 23:43:11 +0800

    initial

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ import requests
 import json
 from urllib import parse
 import instaloader
+import configparser
+
+###### Config ######
+config = configparser.ConfigParser()
+config.read('config.ini')
+tg_token=config['TG']['token']
+ig_username=config['IG']['username']
+ig_password=config['IG']['password']
+###### Config ######
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -15,7 +24,11 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 async def tweet_video(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    url = ' '.join(context.args)
+    logging.info("{} - {}".format('t', update.effective_chat.id))
+    if not context.args:
+        logging.error("{} - {} - {}".format('t', update.effective_chat.id, "Url empty"))
+        return
+    url = ' '.join(context.args) 
     HEADERS = {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8', 'Key': '332213fa4a9d4288b5668ddd9'}
     tvdl = "https://tvdl-api.saif.dev"
     FormData = {"url": url, "ver": "1306"}
@@ -24,31 +37,39 @@ async def tweet_video(update: Update, context: ContextTypes.DEFAULT_TYPE):
     content = json.loads(content)
     videoUrl = content["high"]["downloadURL"]
     await context.bot.send_video(chat_id=update.effective_chat.id, video=videoUrl)
-    logging.info("{} - {}".format('tweet_video', update.effective_chat.id))
 
 async def instagram(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logging.info("{} - {}".format('i', update.effective_chat.id))
+    if not context.args:
+        logging.error("{} - {} - {}".format('i', update.effective_chat.id, "Url empty"))
+        return
     url = ' '.join(context.args)
     array = url.split("/")
     L = instaloader.Instaloader()
-    L.login("wizone_minju_no1", "minju@angel")
-    ##https://www.instagram.com/p/Cgn7fAbviOe/?utm_source=ig_web_copy_link
+    L.login(ig_username, ig_password)
     post = instaloader.Post.from_shortcode(L.context, array[4])
     mediaList = []
-    
-    for slide in post.get_sidecar_nodes():
-        if slide.is_video:
-            mediaList.append(InputMediaVideo(media=slide.video_url))
+
+    if post.mediacount > 1:
+        for slide in post.get_sidecar_nodes():
+            if slide.is_video:
+                mediaList.append(InputMediaVideo(media=slide.video_url))
+            else:
+                mediaList.append(InputMediaPhoto(media=slide.display_url))
+    else:
+        if post.is_video:
+            mediaList.append(InputMediaVideo(media=post.video_url))
         else:
-            mediaList.append(InputMediaPhoto(media=slide.display_url))
-    
+            mediaList.append(InputMediaPhoto(media=post.url))
+
     await context.bot.send_media_group(chat_id=update.effective_chat.id, media=mediaList)
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token('5447887595:AAGOyswoCpGJYCgRwTkmY0u4hV4n2V-w1Oo').build()
+    application = ApplicationBuilder().token(tg_token).build()
     
     start_handler = CommandHandler('start', start)
-    tweet_video_handler = CommandHandler('tweet_video', tweet_video)
-    instagram_handler = CommandHandler('instagram', instagram)
+    tweet_video_handler = CommandHandler('t', tweet_video)
+    instagram_handler = CommandHandler('i', instagram)
 
     application.add_handler(start_handler)
     application.add_handler(tweet_video_handler)
